1d4d7cd70fd4b4eccf7dbb934489505b35967e76
GNT <aznaum@gmail.com>
2020-02-19 22:50:34 -0700

    League work

--- uba.rb
+++ uba.rb
@@ -1,8 +1,41 @@
+require 'securerandom'
+
+class Ballplayer
+
+  attr :name, :pos, :skill
+
+  def initialize(name, pos)
+    @name, @pos = name, pos
+    @skill = (1..4).map { |_| Ballplayer.rand_skill }
+  end
+
+  def self.rand_skill
+    # 0: 125 + 0
+    # 1: 75 + 125
+    # 2: 15 + 75
+    # 3: 1 + 15
+    # 4: 0 + 1
+    x = SecureRandom.rand(432)
+    if x < 125
+      0
+    elsif x < 325
+      1
+    elsif x < 415
+      2
+    elsif x < 431
+      3
+    else
+      4
+    end
+  end
+
+end
+
 class League
 
   attr :cities, :freeagents, :title
 
-  POS_CHART = 'OOOOIIICPPPP'
+  POS_CHART = 'OOOIIIICPPPP'
   TOTAL_TEAMS = 20
 
   def initialize()
@@ -16,6 +49,9 @@ class League
     @freeagents = []
     TOTAL_TEAMS.times do
       @cities << np.draw_loc
+      POS_CHART.each_char do |p|
+        @freeagents << Ballplayer.new(np.draw, p)
+      end
     end
   end
 
